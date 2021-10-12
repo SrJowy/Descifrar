a2a9edb52c8ea8b147937e345564a0cf6cd5ad95
SrJowy <joelbraortiz@gmail.com>
2021-10-12 22:38:44 +0200

    Added comments in esteganografia.sh

--- esteganografia.sh
+++ esteganografia.sh
@@ -4,7 +4,7 @@ sudo apt-get install unzip
 sudo apt-get install stegosuite
 
 unzip imagen.zip
-len=$(ls -l . | egrep -c '*.jpg')
+len=$(ls -l . | egrep -c '*.jpg') #Sacamos la cantidad de archivos jpg que hay después de descomprimir el zip
 h='e5ed313192776744b9b93b1320b5e268'
 i=1
 while [ $i -le $len ]
@@ -13,7 +13,7 @@ do
     ls | egrep $t > log.txt
     if [ $? == 0 ]
     then
-        hash=$(md5sum $t | cut -f 1 -d ' ')
+        hash=$(md5sum $t | cut -f 1 -d ' ') #El comando cut toma el primer campo que da md5sum (el hash) tomando como delimitado (-d) un espacio
         if [ $hash = $h ]
         then 
             stegosuite $t
